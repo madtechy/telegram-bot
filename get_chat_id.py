fe2561e01815d8f26bd61b89534a1c16f23d4e98
madtechy <riteshraj22222@gmail.com>
2025-04-09 05:56:02 +0530

    Initial commit

---
+++ get_chat_id.py
@@ -0,0 +1,7 @@
+import requests
+
+BOT_TOKEN = "7901637126:AAGVBPWFYn1g6PbfLJ6IsbUotzAjeJqgxS0"
+URL = f"https://api.telegram.org/bot{BOT_TOKEN}/getUpdates"
+
+response = requests.get(URL)
+print(response.json())
